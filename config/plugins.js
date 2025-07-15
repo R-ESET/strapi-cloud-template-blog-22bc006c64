d1e61791e2b75221afcae3d8760e90b37dfffe62
shoeb-reset <shoeb@r-eset.com>
2025-07-15 19:45:04 +0530

    privacy screen CMS

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,5 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  'users-permissions': {
+    jwtSecret: env('JWT_SECRET', 'oeseKfu29KSiM6HeiY7angJlX+fGlQDfMgyPS9oBG7s='),
+  },
+});
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+     keys: env.array('APP_KEYS', ['qc4r540EpGIph6DFFBq8vN8ybLHLLxia', 'asuMxXyYx8TGam1cw2zZI8SWRBbdinkd']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
